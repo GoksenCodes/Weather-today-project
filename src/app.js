cf1783c02d794c3cd3cb433395e0ce00659299b2
Goksen <goksengorgulu@gmail.com>
2020-01-09 15:21:37 +0100

    Added current weather icon from API

--- src/app.js
+++ src/app.js
@@ -25,33 +25,22 @@ function formatDate(timestamp) {
     
 }
 
-// let now = new Date();
-
-
-
-// let day = days[now.getDay()];
-
-// let hour = now.getHours();
-// let minutes = now.getMinutes();
-
-// function date() {
-//   document.querySelector("#current-date").innerHTML = `${day}, ${hour}:${minutes}`;
-// }
-// date();
-
 function displayTemprature(response) {
-    console.log(response.data.main.temp)
     let searchedCityTemp = document.querySelector("#temp") ;
     searchedCityTemp.innerHTML = Math.round(response.data.main.temp) ;
     let city = document.querySelector("#searched-city") ;
     city.innerHTML = response.data.name ;
     let currentDate = document.querySelector("#current-date")
     currentDate.innerHTML = formatDate(response.data.dt * 1000);
+    let currentDayIcon = document.querySelector("#current-icon")
+    currentDayIcon.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    currentDayIcon.setAttribute("alt", response.data.weather[0].description);
 }
 
 
 let apiKey="683dab27e61c352cc6f11bcf41592476" ;
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}&units=metric` ;
+let city="Lisbon"
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric` ;
 
 axios.get(apiUrl).then(displayTemprature);
 
